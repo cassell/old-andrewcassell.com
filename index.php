9358782b80f37cc14ede242d4f493a0f259408f9
Andrew Cassell <alc277@gmail.com>
2012-10-25 12:37:17 -0400

    fixed vertical styling at 600px

--- index.php
+++ index.php
@@ -33,49 +33,6 @@ setInterval(function()
 },3000);
 </script>
 <?php
-/*
-
-echo '<div class="container">';
-		
-		echo '<div class="row">';
-		
-			echo '<div class="span3">';
-			
-			echo '<div id="signature"><a href="/">Andrew Cassell</a></div>';
-				echo '<div id="tagline">Web Application Developer<div>Herndon, Virginia</div></div>';
-			
-				$menu = array();
-				//$menu[self::MENU_ABOUT] = array("url" => "/", "alt" => "About" , "img" => "about");
-				$menu[self::MENU_BLOG] = array("url" => $this->getLinkUrl("/blog/"), "text" => "Blog" );
-				$menu[self::MENU_GITHUB] = array("url" => "http://www.github.com/cassell", "text" => "GitHub" , "img" => "github");
-				$menu[self::MENU_TWITTER] = array("url" => "http://www.twitter.com/alc277", "text" => "Twitter" , "img" => "twitter");
-				$menu[self::MENU_FACEBOOK] = array("url" => "http://www.facebook.com/andrewcassell", "text" => "Facebook" , "img" => "facebook");
-				$menu[self::MENU_SWEET_RADISH] = array("url" => "http://www.sweetradish.com", "text" => "SweetRadish" , "img" => "sweetradish");
-				
-				echo '<ul id="menu">';
-				foreach($menu as $key => $item)
-				{
-					echo '<li>';
-					
-						echo '<a href="' . $item['url'] . '">';
-						
-						echo $item['text'];
-
-						
-						echo '</a>';
-						
-					echo '</li>';
-				}
-				
-		echo '</div>';
-			
-		echo '<div class="span9">';
-			echo '<div id="right-content">';
-
-
-
-*/
-//echo '<div class="content photo" style="width:500px;"><img alt="Andrew Cassell" title="Andrew Cassell" src="' . $page->wrapImageS3('/img/andrew-cassell.jpg') . '"></div>';
 
 $page->close();
 
